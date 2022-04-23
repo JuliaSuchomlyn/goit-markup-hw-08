8fc4895d1840f40835bd601e820f9bc3900768ec
Julia <juliasuchomlyn@gmail.com>
2022-04-24 02:39:29 +0300

    header&mobile-menu

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const bodyRef = document.querySelector("[data-body]");
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-        mobileMenuRef.classList.toggle("is-open");
-        bodyRef.classList.toggle("is-scrolling-disabled");
-
-        });
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
 })();
